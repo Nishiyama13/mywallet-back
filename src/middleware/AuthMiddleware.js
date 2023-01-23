b58ea14f5f986c4fe7ef06f75a9451652a976810
Alicia T Nishiyama <nishiyamaeel@gmail.com>
2023-01-22 21:16:46 -0300

    check user session

--- src/middleware/AuthMiddleware.js
+++ src/middleware/AuthMiddleware.js
@@ -4,12 +4,12 @@ export async function authValidation(req, res, next){
     const { authorization} = req.headers;
     const token = authorization?.replace("Bearer", '')
 
-    if(!token) return res.status(422).send("Token invalido, informe o Token corretamente.");
+    if(!token) return res.status(422).send("Token invalido, informe um Token valido.");
 
     try {
         const checkSession = await db.collection("sessions").findOne({ token });
 
-        if(!checkSession) return res.status(401).send("Você não tem autorização para cadastrar uma movimentação em conta");
+        if(!checkSession) return res.status(401).send("Sua sessão expirou, faça login para se conectar");
 
         res.locals.session = checkSession;
 
